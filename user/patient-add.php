1bc1e477568e8b51689533e9a4c603ef5af901f9
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-01-12 20:28:07 +0700

    Update patient-add.php

--- user/patient-add.php
+++ user/patient-add.php
@@ -131,7 +131,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
     <section class="inner-page">
 
     <div class="section-title" data-aos="fade-up">
-          <h2>Regoster Now</h2>
+          <h2>Register Now</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
 
