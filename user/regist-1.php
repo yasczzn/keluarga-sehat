ec0cf6706cb7c3339724e787bbd8378fecb273af
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-01-30 12:01:10 +0700

    300123
    regist-1.php

--- user/regist-1.php
+++ user/regist-1.php
@@ -2,17 +2,17 @@
 
 include '../function/connection.php';
 
-$d = strtotime("today UTC");
+// $d = strtotime("today UTC");
 
-$s1 = mktime(10, 0, 0, $d, $d, 0);
-$s2 = mktime(12, 0, 0, $d, $d, 0);
-$s3 = mktime(14, 0, 0, $d, $d, 0);
-$s4 = mktime(16, 0, 0, $d, $d, 0);
+$s1 = mktime(10);
+$s2 = mktime(12);
+$s3 = mktime(14);
+$s4 = mktime(16);
 
-if ($s1.date("h") <= date("h") || $s2.date("h") <= date("h") || $s3.date("h") <= date("h") || 
-    $s4.date("h") <= date("h")) {
-  $d = strtotime("tomorrow");
-}
+// if ($s1.date("h") <= date("h") || $s2.date("h") <= date("h") || $s3.date("h") <= date("h") || 
+//     $s4.date("h") <= date("h")) {
+//   $d = strtotime("tomorrow");
+// }
 
 $sch1 = date("h.00 a, d M", $s1); 
 $sch2 = date("h.00 a, d M", $s2);
