4772decddc8e95c95c20e21942b897e6f18daa61
yaswu <putriyasminarahmaz@gmail.com>
2023-02-02 15:49:24 +0700

    020223

--- user/regist-1.php
+++ user/regist-1.php
@@ -283,7 +283,7 @@ if (isset($_POST['Submit'])) {
   <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <!-- <script src="../assets/vendor/php-email-form/validate.js"></script> -->
 
   <script>
     function priceTotal(val){
