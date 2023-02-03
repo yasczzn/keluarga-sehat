bf1496f66d5e4ece12d4aef05e76e38330be7ef9
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-02-03 22:35:52 +0700

    030223

--- user/regist-1.php
+++ user/regist-1.php
@@ -26,7 +26,7 @@ if (isset($_POST['Submit'])) {
     $phoneNum = $_POST['phoneNum'];
     $address = $_POST['address'];
     $vaccineType = $_POST['vaccineType'];
-    $vaccinationDate = isset($_POST['vaccinationDate']);
+    $vaccinationDate = $_POST['vaccinationDate'];
     $price = $_POST['price'];
     $payment = $_POST['payment'];
 
@@ -190,7 +190,7 @@ if (isset($_POST['Submit'])) {
               <div class="validate"></div>
             </div>
             <div class="form-floating mb-3">
-              <input type="date" class="form-control form-control-user" value="<?php echo date('Y-m-d', strtotime($vaccinationDate)); ?>" id="vaccinationDate" required>
+              <input type="date" class="form-control form-control-user" value="<?php echo !empty($_POST['vaccinationDate']) ? $_POST['vaccinationDate'] : ''; ?>" id="vaccinationDate" required>
               <label for="inputVaccinationDate">Vaccination Date</label>
               <div class="validate"></div>
             </div>
