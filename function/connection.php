24c0edb0420c63b8aa896c318f4bbebc478d1745
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-02-03 23:08:33 +0700

    030223
    fix conn

--- function/connection.php
+++ function/connection.php
@@ -3,10 +3,10 @@
 $dbhost = $_ENV["MYSQLHOST"];
 $dbuser = $_ENV["MYSQLUSER"];
 $dbpass = $_ENV["MYSQLPASSWORD"];
-$dbname = $_ENV["MYSQLNAME"];
+$dbname = $_ENV["MYSQLDATABASE"];
 $dbport = $_ENV["MYSQLPORT"];
 
-$conn  = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname, $dbport);
+$conn  = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,$dbport);
 
 if($conn->connect_error){
   die('Could not connect connect: '.$conn->connect_error);
