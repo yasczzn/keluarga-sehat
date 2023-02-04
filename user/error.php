5eeb825827b277249eed94f06998c1eaccd86fa5
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-02-04 07:17:49 +0700

    040223

--- user/error.php
+++ user/error.php
@@ -62,13 +62,22 @@
   <main id="main">
 
     <section class="inner-page">
+
       <div class="container">
-        <div class="error">
-            <p>Looks like the page you looking for does not exist...</p>
-            <img src="assets/img/error-404.svg" alt="" class="error-page">
-            <a href="../index.php" class="btn-get-started scrollto">Go back to main page</a>
+        <div class="row">
+          <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="fade-up">
+            <div>
+              <h1>Oops...</h1>
+              <h3>Looks like the page you're looking for does not exist...</h3>
+              <a href="../index.php" class="btn-get-started scrollto">Go Back</a>
+            </div>
+          </div>
+          <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
+            <img src="../assets/img/error-404.svg" class="img-fluid" alt="">
+          </div>
         </div>
       </div>
+      
     </section>
 
   </main><!-- End #main -->
